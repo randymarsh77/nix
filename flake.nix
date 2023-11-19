35ef8f22a5a2703f1779f95bd4e4461dad71b203
randymarsh77 <mattpsbs@gmail.com>
2023-11-19 09:16:46 -0800

    Only filter mysql-workbench in ci configs.

--- flake.nix
+++ flake.nix
@@ -19,15 +19,26 @@
     let
       x86-packages = { pkgs, ... }: {
         # azure-cli is broken on Intel; filter it out.
+        environment.systemPackages = (builtins.filter (x: x != pkgs.azure-cli)
+          base.constants.x86_64-darwin.default-packages) ++ [ pkgs.cachix ];
+      };
+      arm-packages = { pkgs, ... }: {
+        environment.systemPackages =
+          base.constants.aarch64-darwin.default-packages ++ [ pkgs.cachix ];
+      };
+      ci-x86-packages = { pkgs, ... }: {
+        # azure-cli is broken on Intel; filter it out.
         # mysql-workbench-dist can't upload to Cachix; filter it out.
         environment.systemPackages = (builtins.filter (x:
           x != pkgs.azure-cli && x
           != localpkgs.packages.x86_64-darwin.mysql-workbench-dist)
           base.constants.x86_64-darwin.default-packages) ++ [ pkgs.cachix ];
       };
-      arm-packages = { pkgs, ... }: {
-        environment.systemPackages =
-          base.constants.aarch64-darwin.default-packages ++ [ pkgs.cachix ];
+      ci-arm-packages = { pkgs, ... }: {
+        # mysql-workbench-dist can't upload to Cachix; filter it out.
+        environment.systemPackages = (builtins.filter
+          (x: x != localpkgs.packages.x86_64-darwin.mysql-workbench-dist)
+          base.constants.aarch64-darwin.default-packages) ++ [ pkgs.cachix ];
       };
       dotfiles = { home }:
         { pkgs, ... }: {
@@ -52,13 +63,13 @@
         ci-x86 = darwin.lib.darwinSystem {
           modules = [
             (import ./darwin/bootstrap.nix { hostPlatform = "x86_64-darwin"; })
-            x86-packages
+            ci-x86-packages
           ];
         };
         ci-arm = darwin.lib.darwinSystem {
           modules = [
             (import ./darwin/bootstrap.nix { hostPlatform = "aarch64-darwin"; })
-            arm-packages
+            ci-arm-packages
           ];
         };
       };
